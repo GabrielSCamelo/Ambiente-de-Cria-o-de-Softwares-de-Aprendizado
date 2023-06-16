161b0b7bc5a021bd4ea351f71d7144ab1cedc023
GabrielSCamelo <101327825+GabrielSCamelo@users.noreply.github.com>
2023-06-16 09:47:20 -0300

    Update and rename test to Exer01.php

---
+++ PHP/Exer01.php
@@ -0,0 +1,21 @@
+<!DOCKTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+<h1>3DAW</h1>
+<?php
+echo "<h2>Exercicio 02</h2>" ;
+echo "Alo alo bom dia gente" ;
+
+$variavel = "está é uma string";
+echo "<br>";
+echo "$variavel";
+
+$variavel = 3 + 5;
+echo "<br>";
+echo "$variavel";
+
+?>
+</body>
+</html>
